e8c8373bc448fbb708ddb875769982a3ea5eeefc
RoneyThomas <roneythomas6@gmail.com>
2021-12-01 11:28:32 -0500

    Done

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  // Horizontal search
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  // Veritcal search
+  for (let i = 0; i < letters[0].length; i++) {
+    let verticalLine = "";
+    for (let j = 0; j < letters.length; j++) {
+      verticalLine = verticalLine.concat(letters[j][i]);
+    }
+    if (verticalLine.includes(word)) return true;
+  }
+
+  // Diagonal search
+  // let verticalLine = "";
+  // for (let i = 0; i < letters.length; i++) {
+  //   verticalLine = verticalLine.concat(letters[i][i]);
+  // }
+  // if (verticalLine.includes(word)) return true;
+
+  // verticalLine = "";
+  // for (let i = letters.length - 1; i >= 0; i--) {
+  //   verticalLine = verticalLine.concat(letters[i][i]);
+  // }
+  // if (verticalLine.includes(word)) return true;
+
+  return false;
+};
+
+module.exports = wordSearch;
